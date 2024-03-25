7547608c1b479c74e5b24893eb251d9d18850e16
Oleg Bernatskij <oleg.bernatsckij@yandex.ru>
2024-03-25 14:48:41 +0300

    Lesson_12_testng

--- src/main/java/org/aqa/aston/App.java
+++ src/main/java/org/aqa/aston/App.java
@@ -1,13 +1,8 @@
 package org.aqa.aston;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Method method = new Method();
+        System.out.println(method.factorial(5));
     }
 }
--- src/test/java/org/aqa/aston/AppTest.java
+++ src/test/java/org/aqa/aston/AppTest.java
@@ -1,20 +1,14 @@
 package org.aqa.aston;
 
-import static org.junit.Assert.assertTrue;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
 
-import org.junit.Test;
+import static org.testng.AssertJUnit.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test1() {
+        assertEquals(Method.factorial(5), 120);
     }
 }
