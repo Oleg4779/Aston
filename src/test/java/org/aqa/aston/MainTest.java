6d0f6ebfdd9c7fb35d9cb93edc279468d7a584b9
Oleg Bernatskij <oleg.bernatsckij@yandex.ru>
2024-04-01 23:03:23 +0300

    Lesson_14

--- src/test/java/org/aqa/aston/MainTest.java
+++ src/test/java/org/aqa/aston/MainTest.java
@@ -1,12 +1,16 @@
 package org.aqa.aston;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pageEntity.Mts;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.List;
 
 public class MainTest extends BaseTest {
@@ -15,37 +19,40 @@ public class MainTest extends BaseTest {
         Mts mts = PageFactory.initElements(driver, Mts.class);
         driver.get("https://www.mts.by/");
 
-        String title = mts.checkPaymentBlockTitle();
-        Assert.assertEquals(title, "Онлайн пополнение" + "\n" + "без комиссии");
-
-    }
-
-    @Test
-    public void test2() {
-        Mts mts = PageFactory.initElements(driver, Mts.class);
-        driver.get("https://www.mts.by/");
-        List<WebElement> partnerLogoList = driver.findElements(By.xpath("//div[@class='pay__partners']"));
-        Assert.assertFalse(partnerLogoList.isEmpty());
-    }
-
-    @Test
-    public void test3() {
-        Mts mts = PageFactory.initElements(driver, Mts.class);
-        driver.get("https://www.mts.by/");
         mts.cookiesFrameAccepting();
-        mts.moreInfoClick();
-        Assert.assertTrue(driver.getCurrentUrl().contains("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/"));
+
+        mts.checkServicePlaceholders();
+        mts.checkHomeInternetPlaceholders();
+        mts.checkInstalmentPlaceholders();
+        mts.checkArrearsPlaceholders();
     }
 
     @Test
-    public void test4() {
+    public void test2() throws InterruptedException {
         Mts mts = PageFactory.initElements(driver, Mts.class);
-        driver.get("https://www.mts.by/");
-        mts.phoneNumberFieldClick();
-        mts.phoneNumberFieldEntry();
-        mts.amountFieldClick();
-        mts.amountFieldEntry();
-        mts.continueButtonClick();
+        driver.get("https://www.mts.by");
 
+        mts.cookiesFrameAccepting();
+        mts.serviceFieldsFilling();
+        mts.continueButtonClick();
+        Thread.sleep(10000);
+        WebElement iframe = driver.findElement(By.xpath("//iframe[@class='bepaid-iframe']"));
+        driver.switchTo().frame(iframe);
+        WebElement amount = driver.findElement(By.xpath("//div[@class='header__payment-amount']"));
+        Assert.assertTrue(amount.getText().contains("50.00 BYN"));
+        WebElement phoneNumber = driver.findElement(By.xpath("//p[@class='header__payment-info']"));
+        Assert.assertTrue(phoneNumber.getText().contains("375297777777"));
+        WebElement payButton = driver.findElement(By.xpath("//button[@class='colored disabled ng-star-inserted']"));
+        Assert.assertTrue(payButton.getText().contains("50.00 BYN"));
+        WebElement cardNumber = driver.findElement(By.xpath("//label[contains(text(),'Номер карты')]"));
+        WebElement cardDuration = driver.findElement(By.xpath("//label[contains(text(),'Срок действия')]"));
+        WebElement cvc = driver.findElement(By.xpath("//label[contains(text(),'CVC')]"));
+        WebElement cardOwnerName = driver.findElement(By.xpath("//label[contains(text(),'Имя держателя (как на карте)')]"));
+        Assert.assertEquals(cardNumber.getText(), "Номер карты");
+        Assert.assertEquals(cardDuration.getText(), "Срок действия");
+        Assert.assertEquals(cvc.getText(), "CVC");
+        Assert.assertEquals(cardOwnerName.getText(), "Имя держателя (как на карте)");
+        List<WebElement> cardBrands = driver.findElements(By.xpath("//div[@class='cards-brands cards-brands__container ng-tns-c55-0 ng-trigger ng-trigger-brandsState ng-star-inserted']"));
+        Assert.assertFalse(cardBrands.isEmpty());
     }
 }
--- src/test/java/pageEntity/Mts.java
+++ src/test/java/pageEntity/Mts.java
@@ -2,62 +2,121 @@ package pageEntity;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 
 public class Mts {
     @FindBy(xpath = "//div[@class='page-header-links']//a[contains(text(), 'Интернет-магазин')]")
     private WebElement menu;
 
+//    @FindBy(xpath = "//iframe[@class='bepaid-iframe']")
+//    private WebElement paymentForm;
+
     @FindBy(xpath = "//button[text()='Принять']")
     private WebElement buttonAgree;
 
-    @FindBy(xpath = "//h2[text()='Онлайн пополнение ']")
-    private WebElement paymentBlockTitle;
+    @FindBy(xpath = "//button[@class='select__header']")
+    private WebElement paymentFormSelectHeader;
 
-    @FindBy(xpath = "//div[@class='pay__partners']")
-    private WebElement partnerLogo;
+    @FindBy(xpath = "//p[text()='Услуги связи']")
+    private WebElement service;
 
-    @FindBy(xpath = "//section[@class='pay']//a")
-    private WebElement moreInfoLink;
+    @FindBy(xpath = "//p[text()='Домашний интернет']")
+    private WebElement homeInternet;
 
-    @FindBy(xpath = "//div[@class='select__wrapper']")
-    private WebElement paymentFormSelectWrapper;
+    @FindBy(xpath = "//p[text()='Рассрочка']")
+    private WebElement installment;
+
+    @FindBy(xpath = "//p[text()='Задолженность']")
+    private WebElement arrears;
 
     @FindBy(xpath = "//input[@placeholder='Номер телефона']")
-    private WebElement phoneNumber;
+    private WebElement phoneNumberField;
 
-    @FindBy(xpath = "//input[@placeholder='Сумма']")
+    @FindBy(xpath = "//input[@id='internet-phone']")
+    private WebElement internetPhoneNumberField;
+
+    @FindBy(xpath = "//input[@id='score-instalment']")
+    private WebElement scoreInstalmentField;
+
+    @FindBy(xpath = "//input[@id='score-arrears']")
+    private WebElement scoreArrearsField;
+
+    @FindBy(xpath = "//input[@id='connection-sum']")
     private WebElement amountField;
+
+    @FindBy(xpath = "//input[@id='internet-sum']")
+    private WebElement internetAmountField;
+
+    @FindBy(xpath = "//input[@id='instalment-sum']")
+    private WebElement installmentAmountField;
+
+    @FindBy(xpath = "//input[@id='arrears-sum']")
+    private WebElement arrearsAmountField;
+
+    @FindBy(xpath = "//input[@id='connection-email']")
+    private WebElement emailField;
+
+    @FindBy(xpath = "//input[@id='internet-email']")
+    private WebElement internetEmailField;
+
+    @FindBy(xpath = "//input[@id='instalment-email']")
+    private WebElement installmentEmailField;
+
+    @FindBy(xpath = "//input[@id='arrears-email']")
+    private WebElement arrearsEmailField;
 
     @FindBy(xpath = "//form[@class='pay-form opened']//button")
     private WebElement continueButton;
 
+    @FindBy(xpath = "//div[@class='back-container payment-page']")
+    private WebElement paymentWindow;
+
     public void cookiesFrameAccepting() {
-        if(buttonAgree.isDisplayed()) {
+        if (buttonAgree.isDisplayed()) {
             buttonAgree.click();
         }
     }
 
-    public String checkPaymentBlockTitle() {
-        return paymentBlockTitle.getText();
+    public void openDropdownMenu() {
+        paymentFormSelectHeader.click();
     }
 
-    public void moreInfoClick() {
-        moreInfoLink.click();
+    public void checkServicePlaceholders() {
+        openDropdownMenu();
+        service.click();
+        Assert.assertEquals(phoneNumberField.getAttribute("placeholder"), "Номер телефона");
+        Assert.assertEquals(amountField.getAttribute("placeholder"), "Сумма");
+        Assert.assertEquals(emailField.getAttribute("placeholder"), "E-mail для отправки чека");
     }
 
-    public void phoneNumberFieldClick() {
-        phoneNumber.click();
+    public void checkHomeInternetPlaceholders() {
+        openDropdownMenu();
+        homeInternet.click();
+        Assert.assertEquals(internetPhoneNumberField.getAttribute("placeholder"), "Номер абонента");
+        Assert.assertEquals(internetAmountField.getAttribute("placeholder"), "Сумма");
+        Assert.assertEquals(internetEmailField.getAttribute("placeholder"), "E-mail для отправки чека");
     }
 
-    public void phoneNumberFieldEntry() {
-        phoneNumber.sendKeys("297777777");
+    public void checkInstalmentPlaceholders() {
+        openDropdownMenu();
+        installment.click();
+        Assert.assertEquals(scoreInstalmentField.getAttribute("placeholder"), "Номер счета на 44");
+        Assert.assertEquals(installmentAmountField.getAttribute("placeholder"), "Сумма");
+        Assert.assertEquals(installmentEmailField.getAttribute("placeholder"), "E-mail для отправки чека");
     }
 
-    public void amountFieldClick() {
-        amountField.click();
+    public void checkArrearsPlaceholders() {
+        openDropdownMenu();
+        arrears.click();
+        Assert.assertEquals(scoreArrearsField.getAttribute("placeholder"), "Номер счета на 2073");
+        Assert.assertEquals(arrearsAmountField.getAttribute("placeholder"), "Сумма");
+        Assert.assertEquals(arrearsEmailField.getAttribute("placeholder"), "E-mail для отправки чека");
     }
 
-    public void amountFieldEntry() {
+    public void serviceFieldsFilling() {
+        openDropdownMenu();
+        service.click();
+        phoneNumberField.sendKeys("297777777");
         amountField.sendKeys("50");
     }
 
@@ -65,3 +124,5 @@ public class Mts {
         continueButton.click();
     }
 }
+
+
